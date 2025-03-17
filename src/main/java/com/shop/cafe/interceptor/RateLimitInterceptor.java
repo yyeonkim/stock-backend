10b918fac07b17cf8e65611e850de47e7862be67
Yongyeon Kim <seyeon.dev@gmail.com>
2025-03-17 17:46:19 +0900

    feat: API 호출 횟수 제한 로직 구현

--- src/main/java/com/shop/cafe/interceptor/RateLimitInterceptor.java
+++ src/main/java/com/shop/cafe/interceptor/RateLimitInterceptor.java
@@ -2,6 +2,9 @@ package com.shop.cafe.interceptor;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -11,15 +14,34 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class RateLimitInterceptor implements HandlerInterceptor {
-	private static final int MAX_REQUESTS = 10;  // 허용 요청 횟수
-    private static final int TIME_WINDOW = 5;   // 제한 시간(초)
+	// 5초 동안 10회 허용
+	private static final int MAX_REQUESTS = 10;  
+    private static final int TIME_WINDOW = 5;   
     
 	private final Map<String, Integer> requestCounts = new ConcurrentHashMap<>();
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+	
+	public RateLimitInterceptor() {
+        // TIME_WINDOW(초)마다 요청 카운트 기록 초기화
+        scheduler.scheduleAtFixedRate(requestCounts::clear, TIME_WINDOW, TIME_WINDOW, TimeUnit.SECONDS);
+    }
 	 
 	@Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        
-        System.out.println("Hi, I'm interceptor.");
+		String clientIP = request.getRemoteAddr();
+		
+		requestCounts.putIfAbsent(clientIP, 0); // 초기화
+		
+		int currCount = requestCounts.get(clientIP);
+		
+		// 요청 횟수 초과
+		if (currCount >= MAX_REQUESTS) {
+			response.sendError(429, "Blocked. Retry later.");
+			return false;
+		}
+		
+		requestCounts.put(clientIP, currCount + 1);
+		System.out.println("API count: " + (currCount + 1));
         
         return true;
     }  
