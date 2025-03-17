5694dd8d3ddad851e8f831860582d28e8fd7bc9e
Yongyeon Kim <seyeon.dev@gmail.com>
2025-03-17 20:45:54 +0900

    chore: 불필요한 import 삭제

--- src/main/java/com/shop/cafe/controller/WalletController.java
+++ src/main/java/com/shop/cafe/controller/WalletController.java
@@ -1,11 +1,8 @@
 package com.shop.cafe.controller;
 
-import com.shop.cafe.dto.Wallet;
 import com.shop.cafe.service.WalletService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
